22a8df53f98ef404c7cdda46469e43aa83f11500
jerxmyy <jeremy.chambon2906@gmail.com>
2026-02-06 22:55:38 +0100

    modif de l'adresse mail

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -80,7 +80,7 @@
                   d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"
                 />
               </svg>
-              jeremy.chambon@mail-esd.com
+              jeremy.chambon2906@gmail.com
             </p>
             <p>
               <svg
